172a1856fed768388ca1dbc23914b3b5fddf034b
devadath-2004 <devan2004nair@gmail.com>
2026-01-30 10:11:57 +0530

    Frontend UI for Face Attendance System

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Webcam Preview button (demo action)
+const enableBtn = document.getElementById("enableCameraBtn");
+
+enableBtn.addEventListener("click", () => {
+  alert("Webcam preview will be enabled on the Attendance page!");
+});
